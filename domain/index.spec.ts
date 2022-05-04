1c596ebba39628bf14fa891b019a8da1b1f7318d
Mathieu <mathieu.parinet@gmail.com>
2022-05-04 13:23:10 +0200

    add NEGATE

--- domain/index.spec.ts
+++ domain/index.spec.ts
@@ -38,6 +38,9 @@ it("Test a caclul with operator *", function () {
 it("Test a negative result", function () {
   expect(polishReverse("24-")).toEqual(-2);
 });
+it("Test a negative number", function () {
+  expect(polishReverse("1NEGATE")).toEqual(-1);
+});
 it("Test a complexe calcul RNP", function () {
   expect(polishReverse("11+4*2/22++")).toEqual(8);
 });
--- domain/index.ts
+++ domain/index.ts
@@ -1,4 +1,4 @@
-type Operator = "+" | "*" | "-" | "/"; 
+type Operator = "+" | "*" | "-" | "/" | "N"; 
 
 export function calculateAddition(a:number, b:number):number {
   return a + b
@@ -12,6 +12,9 @@ export function calculateDivision(a:number, b:number):number {
 export function calculateMultiplication(a:number, b:number):number {
   return a * b
 }
+export function calculateNegation(a:number):number {
+  return -a
+}
 
 export function calculate(a:number, op:Operator, b:number):number {
   switch (op) {
@@ -23,24 +26,32 @@ export function calculate(a:number, op:Operator, b:number):number {
           return calculateSoustraction(a, b);
       case "/":
           return calculateDivision(a, b);
+      case "N":
+        return calculateNegation(a);
       default:
           break;
   }
 }
 
 function idk(tab:Array<any>, i:number):Array<any> {
-  let tmp = tab.slice(i-2,i)
-  tab[i-2] = calculate(Number(tmp[0]), tab[i], Number(tmp[1]))
-  tab[i-1] = tab[i] = ""
-  console.log(tab.filter(e => e !== ""))
-  return tab.filter(e => e !== "")
+  if(tab[i] === "N"){
+    tab[i-1] = calculate(Number(tab[i-1]),"N",0)
+    tab.splice(i,i+5)
+  }else{
+    let tmp = tab.slice(i-2,i)
+    tab[i-2] = calculate(Number(tmp[0]), tab[i], Number(tmp[1]))
+    tab[i-1] = tab[i] = ""
+    tab.splice(i-1,2)
+  }
+
+  return tab
 }
 
 export function polishReverse(text:string):string {
   let tab = text.split("")
-
-  while( tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-") !== -1) {
-      let i = tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-")
+  
+  while( tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-" || e === "N" ) !== -1) {
+      let i = tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-" || e === "N")
       tab = idk(tab,i)
   }
 
