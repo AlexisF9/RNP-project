778b361154dfe3f186eedfd8be79fbc2f0d30fd7
Alexis Flacher <alexis.flacher38@gmail.com>
2022-05-04 10:44:37 +0200

    add rnp calcul

--- domain/index.spec.ts
+++ domain/index.spec.ts
@@ -1,5 +1,44 @@
-import { test } from ".";
+import { 
+  calculateAddition, 
+  calculateSoustraction, 
+  calculateDivision, 
+  calculateMultiplication,
+  calculate, 
+  polishReverse
+} from ".";
 
-it("That's a test!", function () {
-  expect(test()).toEqual(5);
+it("Test with operator +", function () {
+  expect(calculateAddition(1, 1)).toEqual(2);
 });
+it("Test with operator -", function () {
+  expect(calculateSoustraction(4, 2)).toEqual(2);
+});
+it("Test with operator /", function () {
+  expect(calculateDivision(4, 2)).toEqual(2);
+});
+it("Test with operator *", function () {
+  expect(calculateMultiplication(4, 2)).toEqual(8);
+});
+
+
+it("Test a caclul with operator +", function () {
+  expect(calculate(1, "+", 1)).toEqual(2);
+});
+it("Test a caclul with operator -", function () {
+  expect(calculate(1, "-", 1)).toEqual(0);
+});
+it("Test a caclul with operator /", function () {
+  expect(calculate(4, "/", 2)).toEqual(2);
+});
+it("Test a caclul with operator *", function () {
+  expect(calculate(1, "*", 1)).toEqual(1);
+});
+
+
+it("Test a negative result", function () {
+  expect(polishReverse("24-")).toEqual(-2);
+});
+it("Test a complexe calcul RNP", function () {
+  expect(polishReverse("11+4*2/22++")).toEqual(8);
+});
+
--- domain/index.ts
+++ domain/index.ts
@@ -1,5 +1,48 @@
-export const test = () => {
-  console.log("Hello");
+type Operator = "+" | "*" | "-" | "/"; 
 
-  return 5;
-};
+export function calculateAddition(a:number, b:number):number {
+  return a + b
+}
+export function calculateSoustraction(a:number, b:number):number {
+  return a - b
+}
+export function calculateDivision(a:number, b:number):number {
+  return a / b
+}
+export function calculateMultiplication(a:number, b:number):number {
+  return a * b
+}
+
+export function calculate(a:number, op:Operator, b:number):number {
+  switch (op) {
+      case "+":
+          return calculateAddition(a, b);
+      case "*":
+          return calculateMultiplication(a, b);
+      case "-":
+          return calculateSoustraction(a, b);
+      case "/":
+          return calculateDivision(a, b);
+      default:
+          break;
+  }
+}
+
+function idk(tab:Array<any>, i:number):Array<any> {
+  let tmp = tab.slice(i-2,i)
+  tab[i-2] = calculate(Number(tmp[0]), tab[i], Number(tmp[1]))
+  tab[i-1] = tab[i] = ""
+  console.log(tab.filter(e => e !== ""))
+  return tab.filter(e => e !== "")
+}
+
+export function polishReverse(text:string):string {
+  let tab = text.split("")
+
+  while( tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-") !== -1) {
+      let i = tab.findIndex(e => e === "*"  || e === "/" || e === "+" || e === "-")
+      tab = idk(tab,i)
+  }
+
+  return tab[0]
+}
